c7933e858347095c5681e1205aace282e86824b3
Chassity <122334539+Chassityb@users.noreply.github.com>
2024-07-16 17:15:11 -0700

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,9 @@
+
+# I like to play video games
+print ("CCCCC    BBBBBB") 
+print ("C        B    B")
+print ("C        B    B")
+print ("C        BBBBBB")
+print ("C        B    B")
+print ("C        B    B")
+print ("CCCCC    BBBBBB")
