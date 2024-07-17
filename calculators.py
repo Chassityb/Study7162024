9bc49e2b6dfaa5e7b3e5e54fef1b08c85ee877b5
Chassity <122334539+Chassityb@users.noreply.github.com>
2024-07-16 17:58:06 -0700

    Add files via upload

---
+++ calculators.py
@@ -0,0 +1,19 @@
+# This is a pythagorean theorem calculator!
+
+
+a = int(input("ENTER A"))
+b = int(input("ENTER B"))
+print ((a ** 2 + b ** 2) **.5)
+
+# This is a Farenheight to Celcius calculator!
+
+f = int(input("Enter Farenheight"))
+print ((f - 32) / 1.8)
+
+# This is a currency exchange calculator!
+
+pesos = int(input('What do you have left in pesos?'))
+soles = int(input("What do you have left in soles?"))
+reais = int(input("What do you have left in reais?"))
+
+print ((pesos * 0.057) + (soles * 0.27) + (reais * 0.18))
